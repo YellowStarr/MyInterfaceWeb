e2d3f5298fcf1d9ce35dcb13f3b8ec4ce81c71a8
YellowStarr <sillyapplemi@126.com>
2017-08-01 18:03:08 +0800

    修改表格样式，添加用例展示

--- sign/views.py
+++ sign/views.py
@@ -6,9 +6,12 @@ from models import Testcase
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+#首页入口，展示用例
 def index(request):
     if request.method=="GET":
-        return render(request, 'index.html')
+        c = selectCase()
+        print c
+        return render(request, 'index.html',{"case":c})
     elif request.method=="POST":
         r=request.POST.get("uname","")
         return render(request,"index.html",{'w':r})
@@ -17,18 +20,24 @@ def inserttestcase(request):
     if request.method=="GET":
         return render(request,"inserttestcase.html")
     elif request.method == "POST":
+        name = request.POST.get("desc","")
+        n=name.encode('utf-8')
         baseurl = request.POST.get("baseurl","")
         url = request.POST.get("url","")
         args = request.POST.get("args","")
         methods = request.POST.get("method","")
         # arg=eval(args)
         print "-----------"+baseurl
-        r={"baseurl":baseurl,"url":url,"args":args,'method':methods}
+        r={"name":n,"baseurl":baseurl,"url":url,"args":args,'method':methods}
         # if not type(r):
         insertCase(r)
         return render(request, "inserttestcase.html")
 
 def insertCase(data):
-    case = Testcase(baseurl=data['baseurl'],url=data['url'],args=data['args'],method=data['method'])
+    case = Testcase(name=data['name'],baseurl=data['baseurl'],url=data['url'],args=data['args'],method=data['method'])
     case.save()
     print "insert"
+
+def selectCase():
+    cases = Testcase.objects.all()
+    return cases
